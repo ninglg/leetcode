b0c60ca0aa237beb174c5e0787ee792a3b3e9568
ninglg <ninglg@gmail.com>
2019-08-08 12:28:47 +0800

    update

---
+++ 70.爬楼梯.go
@@ -0,0 +1,13 @@
+func climbStairs(n int) int {
+    if (n == 0 || n == 1) {
+        return n
+    }
+    res := make([]int,n + 1)
+    res[0] = 1;
+    res[1] = 1;
+    for i := 2; i <= n; i++ {
+        res[i] = res[i-1] + res[i-2]
+    }
+    return res[n]
+}
+

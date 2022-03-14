e796d0c9667b89e7ceae4072bac1d420835a9fd6
ninglg <ninglg@gmail.com>
2022-03-14 11:39:50 +0800

    update

---
+++ 114_flatten-binary-tree-to-linked-list.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flatten(root *TreeNode) {
+	node := root
+
+	for node != nil {
+		if node.Left != nil {
+			tmp := node.Left
+
+			for tmp.Right != nil {
+				tmp = tmp.Right
+			}
+
+			tmp.Right = node.Right
+			node.Right = node.Left
+			node.Left = nil
+		}
+        
+		node = node.Right
+	}
+}
+

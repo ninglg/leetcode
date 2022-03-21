28724ba9bac7d2965e9f071d441bdd85958c8d77
ninglg <ninglg@gmail.com>
2022-03-21 13:56:04 +0800

    update

---
+++ 56_shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.go
@@ -0,0 +1,15 @@
+func singleNumber(nums []int) int {
+    m := make(map[int]int)
+
+    for _, v := range nums {
+        m[v]++
+    }
+
+    for k, v := range m {
+        if v == 1 {
+            return k
+        }
+    }
+
+    return 0
+}

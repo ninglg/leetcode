a29942b7b03dc8189f04c664708884be6ccc9fbd
ninglg <ninglg@gmail.com>
2022-03-22 20:00:33 +0800

    update

---
+++ 622_design-circular-queue.go
@@ -0,0 +1,77 @@
+type MyCircularQueue struct {
+    queue []int
+    front, rear int
+    size int
+}
+
+
+func Constructor(k int) MyCircularQueue {
+    return MyCircularQueue{
+        queue: make([]int, k + 1, k + 1),
+        front: 0,
+        rear: 0,
+        size: k + 1,
+    }
+}
+
+
+func (this *MyCircularQueue) EnQueue(value int) bool {
+    if this.IsFull() {
+        return false
+    }
+
+    this.queue[this.rear] = value
+    this.rear = (this.rear + 1 + this.size) % this.size
+
+    return true
+}
+
+
+func (this *MyCircularQueue) DeQueue() bool {
+    if this.IsEmpty() {
+        return false
+    }
+
+    this.front = (this.front + 1 + this.size) % this.size
+
+    return true
+}
+
+
+func (this *MyCircularQueue) Front() int {
+    if this.IsEmpty() {
+        return -1
+    }
+    return this.queue[this.front]
+}
+
+
+func (this *MyCircularQueue) Rear() int {
+    if this.IsEmpty() {
+        return -1
+    }
+    return this.queue[(this.rear - 1 + this.size) % this.size]
+}
+
+
+func (this *MyCircularQueue) IsEmpty() bool {
+    return this.front == this.rear
+}
+
+
+func (this *MyCircularQueue) IsFull() bool {
+    return (this.rear + 1) % this.size == this.front
+}
+
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.EnQueue(value);
+ * param_2 := obj.DeQueue();
+ * param_3 := obj.Front();
+ * param_4 := obj.Rear();
+ * param_5 := obj.IsEmpty();
+ * param_6 := obj.IsFull();
+ */
+
